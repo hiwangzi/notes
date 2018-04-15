ca6107de614a219756ff0d764c01200d8b6c9fd6
zill057 <hiwangzi@outlook.com>
2018-04-15 20:16:59 +0800

    cnblogs《Entity Framework 新手入门友好实例》

---
+++ DB/ORM/resources/Sstudent/UI/Signup.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace UI
+{
+    partial class Signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Signup = new System.Windows.Forms.Button();
+            this.textBox_Hobby = new System.Windows.Forms.TextBox();
+            this.textBox_Address = new System.Windows.Forms.TextBox();
+            this.textBox_PhoneNumber = new System.Windows.Forms.TextBox();
+            this.textBox_Sex = new System.Windows.Forms.TextBox();
+            this.textBox_FullName = new System.Windows.Forms.TextBox();
+            this.textBox_StudentNumber = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.textBox_Username = new System.Windows.Forms.TextBox();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.label_Username = new System.Windows.Forms.Label();
+            this.label_PhoneNumber = new System.Windows.Forms.Label();
+            this.label_Sex = new System.Windows.Forms.Label();
+            this.label_Hobby = new System.Windows.Forms.Label();
+            this.label_FullName = new System.Windows.Forms.Label();
+            this.label_Address = new System.Windows.Forms.Label();
+            this.label_StudentNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Signup
+            // 
+            this.button_Signup.Location = new System.Drawing.Point(104, 400);
+            this.button_Signup.Name = "button_Signup";
+            this.button_Signup.Size = new System.Drawing.Size(75, 23);
+            this.button_Signup.TabIndex = 31;
+            this.button_Signup.Text = "注册";
+            this.button_Signup.UseVisualStyleBackColor = true;
+            this.button_Signup.Click += new System.EventHandler(this.button_Signup_Click);
+            // 
+            // textBox_Hobby
+            // 
+            this.textBox_Hobby.Location = new System.Drawing.Point(119, 340);
+            this.textBox_Hobby.Name = "textBox_Hobby";
+            this.textBox_Hobby.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Hobby.TabIndex = 30;
+            // 
+            // textBox_Address
+            // 
+            this.textBox_Address.Location = new System.Drawing.Point(119, 297);
+            this.textBox_Address.Name = "textBox_Address";
+            this.textBox_Address.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Address.TabIndex = 29;
+            // 
+            // textBox_PhoneNumber
+            // 
+            this.textBox_PhoneNumber.Location = new System.Drawing.Point(119, 254);
+            this.textBox_PhoneNumber.Name = "textBox_PhoneNumber";
+            this.textBox_PhoneNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBox_PhoneNumber.TabIndex = 28;
+            // 
+            // textBox_Sex
+            // 
+            this.textBox_Sex.Location = new System.Drawing.Point(119, 213);
+            this.textBox_Sex.Name = "textBox_Sex";
+            this.textBox_Sex.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Sex.TabIndex = 27;
+            // 
+            // textBox_FullName
+            // 
+            this.textBox_FullName.Location = new System.Drawing.Point(119, 168);
+            this.textBox_FullName.Name = "textBox_FullName";
+            this.textBox_FullName.Size = new System.Drawing.Size(100, 20);
+            this.textBox_FullName.TabIndex = 26;
+            // 
+            // textBox_StudentNumber
+            // 
+            this.textBox_StudentNumber.Location = new System.Drawing.Point(119, 125);
+            this.textBox_StudentNumber.Name = "textBox_StudentNumber";
+            this.textBox_StudentNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBox_StudentNumber.TabIndex = 25;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(119, 82);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Password.TabIndex = 24;
+            // 
+            // textBox_Username
+            // 
+            this.textBox_Username.Location = new System.Drawing.Point(119, 37);
+            this.textBox_Username.Name = "textBox_Username";
+            this.textBox_Username.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Username.TabIndex = 23;
+            // 
+            // label_Password
+            // 
+            this.label_Password.AutoSize = true;
+            this.label_Password.Location = new System.Drawing.Point(70, 85);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(43, 13);
+            this.label_Password.TabIndex = 22;
+            this.label_Password.Text = "密码：";
+            // 
+            // label_Username
+            // 
+            this.label_Username.AutoSize = true;
+            this.label_Username.Location = new System.Drawing.Point(58, 40);
+            this.label_Username.Name = "label_Username";
+            this.label_Username.Size = new System.Drawing.Size(55, 13);
+            this.label_Username.TabIndex = 21;
+            this.label_Username.Text = "用户名：";
+            // 
+            // label_PhoneNumber
+            // 
+            this.label_PhoneNumber.AutoSize = true;
+            this.label_PhoneNumber.Location = new System.Drawing.Point(70, 257);
+            this.label_PhoneNumber.Name = "label_PhoneNumber";
+            this.label_PhoneNumber.Size = new System.Drawing.Size(43, 13);
+            this.label_PhoneNumber.TabIndex = 20;
+            this.label_PhoneNumber.Text = "电话：";
+            // 
+            // label_Sex
+            // 
+            this.label_Sex.AutoSize = true;
+            this.label_Sex.Location = new System.Drawing.Point(70, 216);
+            this.label_Sex.Name = "label_Sex";
+            this.label_Sex.Size = new System.Drawing.Size(43, 13);
+            this.label_Sex.TabIndex = 19;
+            this.label_Sex.Text = "性别：";
+            // 
+            // label_Hobby
+            // 
+            this.label_Hobby.AutoSize = true;
+            this.label_Hobby.Location = new System.Drawing.Point(70, 343);
+            this.label_Hobby.Name = "label_Hobby";
+            this.label_Hobby.Size = new System.Drawing.Size(43, 13);
+            this.label_Hobby.TabIndex = 18;
+            this.label_Hobby.Text = "爱好：";
+            // 
+            // label_FullName
+            // 
+            this.label_FullName.AutoSize = true;
+            this.label_FullName.Location = new System.Drawing.Point(70, 171);
+            this.label_FullName.Name = "label_FullName";
+            this.label_FullName.Size = new System.Drawing.Size(43, 13);
+            this.label_FullName.TabIndex = 17;
+            this.label_FullName.Text = "姓名：";
+            // 
+            // label_Address
+            // 
+            this.label_Address.AutoSize = true;
+            this.label_Address.Location = new System.Drawing.Point(70, 300);
+            this.label_Address.Name = "label_Address";
+            this.label_Address.Size = new System.Drawing.Size(43, 13);
+            this.label_Address.TabIndex = 16;
+            this.label_Address.Text = "地址：";
+            // 
+            // label_StudentNumber
+            // 
+            this.label_StudentNumber.AutoSize = true;
+            this.label_StudentNumber.Location = new System.Drawing.Point(70, 128);
+            this.label_StudentNumber.Name = "label_StudentNumber";
+            this.label_StudentNumber.Size = new System.Drawing.Size(43, 13);
+            this.label_StudentNumber.TabIndex = 15;
+            this.label_StudentNumber.Text = "学号：";
+            // 
+            // Signup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 460);
+            this.Controls.Add(this.button_Signup);
+            this.Controls.Add(this.textBox_Hobby);
+            this.Controls.Add(this.textBox_Address);
+            this.Controls.Add(this.textBox_PhoneNumber);
+            this.Controls.Add(this.textBox_Sex);
+            this.Controls.Add(this.textBox_FullName);
+            this.Controls.Add(this.textBox_StudentNumber);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.textBox_Username);
+            this.Controls.Add(this.label_Password);
+            this.Controls.Add(this.label_Username);
+            this.Controls.Add(this.label_PhoneNumber);
+            this.Controls.Add(this.label_Sex);
+            this.Controls.Add(this.label_Hobby);
+            this.Controls.Add(this.label_FullName);
+            this.Controls.Add(this.label_Address);
+            this.Controls.Add(this.label_StudentNumber);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Signup";
+            this.Text = "Signup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Signup;
+        private System.Windows.Forms.TextBox textBox_Hobby;
+        private System.Windows.Forms.TextBox textBox_Address;
+        private System.Windows.Forms.TextBox textBox_PhoneNumber;
+        private System.Windows.Forms.TextBox textBox_Sex;
+        private System.Windows.Forms.TextBox textBox_FullName;
+        private System.Windows.Forms.TextBox textBox_StudentNumber;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.TextBox textBox_Username;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.Label label_Username;
+        private System.Windows.Forms.Label label_PhoneNumber;
+        private System.Windows.Forms.Label label_Sex;
+        private System.Windows.Forms.Label label_Hobby;
+        private System.Windows.Forms.Label label_FullName;
+        private System.Windows.Forms.Label label_Address;
+        private System.Windows.Forms.Label label_StudentNumber;
+    }
+}
