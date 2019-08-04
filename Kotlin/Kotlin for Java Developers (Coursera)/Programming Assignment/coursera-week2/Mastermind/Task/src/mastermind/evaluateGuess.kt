2444d1ced28426fb98c36433cffec959f6e83792
Luke Wang <me@lukewang.dev>
2019-08-04 18:09:12 +0800

    Rewrite Programming Assignment (Week2) for Kotlin learning in functional style

--- Kotlin/Kotlin for Java Developers (Coursera)/Programming Assignment/coursera-week2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ Kotlin/Kotlin for Java Developers (Coursera)/Programming Assignment/coursera-week2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -4,32 +4,18 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     if (secret.length != guess.length) throw IllegalArgumentException("secret.length != guess.length")
-    val commonElements = mutableListOf<Char>()
-    for (char in guess) {
-        if (char in secret && char !in commonElements) commonElements.add(char)
-    }
 
-    var rightPosition = 0
+    val rightPosition = guess.withIndex().filter { it.value == secret[it.index] }.size
+
+    // 战五渣的我想不出别的做法了
+    val notRightIndexes = guess.withIndex().filter { it.value != secret[it.index] }.map { it.index }
+    val notRightSecret = secret.withIndex().filter { notRightIndexes.contains(it.index) }.map { it.value }.sorted()
+    val notRightGuess = guess.withIndex().filter { notRightIndexes.contains(it.index) }.map { it.value }.sorted()
+    val intersectionElements = notRightGuess.filter { notRightGuess.contains(it) }.distinct()
     var wrongPosition = 0
-    val secretList = secret.toMutableList()
-    val guessList = guess.toMutableList()
-    for (element in guess.withIndex()) {
-        if (element.value in commonElements) {
-            if (element.value == secret[element.index]) {
-                secretList.remove(element.value)
-                guessList.remove(element.value)
-                rightPosition += 1
-            }
-        }
-    }
-    for (element in guess.withIndex()) {
-        if (element.value in commonElements) {
-            if (element.value in secretList && element.value in guessList) {
-                secretList.remove(element.value)
-                guessList.remove(element.value)
-                wrongPosition += 1
-            }
-        }
+    for (element in intersectionElements) {
+        wrongPosition +=
+                (listOf(notRightSecret.count { it == element }, notRightGuess.count { it == element }).min() ?: 0)
     }
     return Evaluation(rightPosition, wrongPosition)
 }
